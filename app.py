18db8592fdf342fcbe1a8ef85fe9760096e241c2
walterblacknew <159913184+walterblacknew@users.noreply.github.com>
2025-04-12 22:22:09 +0330

    Update app.py

--- app.py
+++ app.py
@@ -6070,7 +6070,7 @@ def create_app():
             return jsonify({'error': str(e)}), 500
 
     return app
-
+    
 if __name__ == '__main__':
    application = create_app()
-   application.run(debug=True, port=8000)
+   application.run(debug=True, host='0.0.0.0', port=8000)
